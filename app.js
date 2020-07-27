ccfe5a7a3c0371e2655a795832072caa92f62dda
Siddharth Kumar <67679454+Siddharth453@users.noreply.github.com>
2020-07-27 20:53:01 +0530

    Add all files of project

---
+++ app.js
@@ -0,0 +1,38 @@
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('form')
+})
+app.post('/result', (req, res) => {
+    var options = {
+        method: 'GET',
+        url: 'https://covid-19-data.p.rapidapi.com/country',
+        qs: { format: 'json', name: req.body.country },
+        headers: {
+            'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
+            'x-rapidapi-key': 'e70e35986dmshc210e9f2a9a48cep1ba473jsn1be593472af3',
+            useQueryString: true
+        }
+    };
+    request(options, function (error, response, body) {
+        if (error) {
+            res.redirect('/')
+        }
+            const parsedBody = JSON.parse(body);
+            res.render('result', {
+                body: parsedBody
+            })
+    });
+})
+app.get('*', (req, res) => {
+    res.render('404');
+})
+app.listen(process.env.PORT || 4001, process.env.IP, () => {
+    console.log('Server is running on PORT 4001!')
+})
